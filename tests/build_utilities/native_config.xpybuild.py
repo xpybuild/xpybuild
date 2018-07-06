bf599891c0823a5625a6a7c8aab64bbcbdee1317
Ben Spiller <ben-spiller@users.noreply.github.com>
2018-07-06 18:38:28 +0100

    add basic C++ compilation test

---
+++ tests/build_utilities/native_config.xpybuild.py
@@ -0,0 +1,19 @@
+import os
+from propertysupport import *
+from buildcommon import *
+from pathsets import *
+from utils.compilers import GCC, VisualStudio
+
+# some basic defaults for recent default compilers for running our testcases with
+if isWindows():
+	VSROOT=r'c:\Program Files (x86)\Microsoft Visual Studio 14.0'
+	assert os.path.exists(VSROOT), 'Cannot find Visual Studio installed in: %s'%VSROOT
+	setGlobalOption('native.include', [r"%s\VC\ATLMFC\INCLUDE" % VSROOT, r"%s\VC\INCLUDE" % VSROOT, r"C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\ucrt"])
+	setGlobalOption('native.libpaths', [r"%s\VC\ATLMFC\LIB\amd64" % VSROOT, r"%s\VC\LIB\amd64" % VSROOT, r"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.10240.0\ucrt\x64", r"C:\Program Files (x86)\Windows Kits\8.1\Lib\winv6.3\um\x64"])
+	setGlobalOption('native.compilers', VisualStudio(VSROOT+r'\VC\bin\amd64'))
+	setGlobalOption('native.cxx.flags', ['/EHa', '/GR', '/O2', '/Ox', '/Ot', '/MD', '/nologo'])
+	setGlobalOption('native.cxx.path', ["%s\Common7\IDE" % VSROOT,r"%s\VC\BIN\amd64" % VSROOT, "%s\Common7\Tools" % VSROOT, r"c:\Windows\Microsoft.NET\Framework\v3.5"])
+	
+else:
+	setGlobalOption('native.compilers', GCC())
+	setGlobalOption('native.cxx.flags', ['-fPIC', '-O3', '--std=c++0x'])
---
+++ tests/targets/CppCompilation/run.py
@@ -0,0 +1,13 @@
+from pysys.constants import *
+from xpybuild.xpybuild_basetest import XpybuildBaseTest
+
+class PySysTest(XpybuildBaseTest):
+
+	def execute(self):
+		msg = self.xpybuild(shouldFail=False, args=[], buildfile='root.xpybuild.py')
+		self.startProcess(self.output+'/build-output/test', [], stdout='test.out', stderr='test.err')
+
+	def validate(self):
+		self.assertGrep(file='xpybuild.out', expr="ERROR .*", contains=False)
+		self.assertGrep(file='test.out', expr="Got string: <STRING1STRING2>")
+		
