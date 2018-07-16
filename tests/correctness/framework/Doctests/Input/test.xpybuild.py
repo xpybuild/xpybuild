03b072eda5d1f08e004f679560b9e8bda07a2f65
Ben Spiller <ben-spiller@users.noreply.github.com>
2018-07-17 00:01:43 +0100

    add pysys test that executes doctests

---
+++ tests/correctness/framework/Doctests/Input/test.xpybuild.py
@@ -0,0 +1,83 @@
+import os, logging
+from propertysupport import *
+from buildcommon import *
+from pathsets import *
+
+defineOutputDirProperty('OUTPUT_DIR', None)
+
+import basetarget 
+
+defineStringProperty('MY_PROP', 'expectedval')
+
+defineOption('testoption.default', '${MY_PROP}')
+defineOption('testoption.globalOverride', 'unexpectedval')
+setGlobalOption('testoption.globalOverride', 'expectedval')
+
+defineOption('testoption.targetOverride', 'defaultval')
+
+defineOption('testoption.legacyTargetOverride', 'defaultval')
+
+defineOption('testoption2.empty', '')
+
+class CustomPathSet(PathSet):
+	def __init__(self, *args, **kwargs):
+		PathSet.__init__(self, *args, **kwargs)
+	def resolveWithDestinations(self, context):
+		assert self.target.options
+		return PathSet.resolveWithDestinations(self, context)
+	def _resolveUnderlyingDependencies(self, context):
+		# this might be needed in some targets, so check it works
+		assert self.target.options
+		logging.getLogger('custompathset').critical('PathSet._resolveUnderlyingDependencies got options: %s', self.target.options)
+		return PathSet._resolveUnderlyingDependencies(self, context)
+
+class MyTarget(basetarget.BaseTarget):
+	def __init__(self, name, options=None, deps=[]):
+		super(MyTarget, self).__init__(name, dependencies=deps)
+		
+		if options:
+			# this is no longer recommended practice, but need to maintain it for backwards compat
+			self.options = options
+
+		try:
+			x = self.options
+			self.log.error('ERROR - should be impossible to read self.options in the constructor unexpectedly: %s', x)
+		except Exception, e:
+			self.log.critical('Exception from trying to read self.options: %s'%e)
+		
+	def run(self, context):
+		name = os.path.basename(self.path)
+		
+		with open(self.path, 'w') as f:
+			def printit(o, display):
+				for k in sorted(o.keys()):
+					if k.startswith('testoption.'): 
+						self.log.critical('-- %s %s=%s', display, k, o[k])
+						print >>f, '%s %s=%s'%(display, k, o[k])
+		
+			printit(context.mergeOptions(self), name+' mergeOptions')
+			assert context.mergeOptions(self) == self.options, '%s != %s'%(context.mergeOptions(self), self.options)
+			self.log.critical('-- %s getOption %s=%s', name, 'testoption.default', self.getOption('testoption.default'))
+			
+			try:
+				self.getOption('testoption2.empty')
+				self.log.error('ERROR - getOption should throw if empty')
+			except Exception, e:
+				self.log.critical('Got expected exception: %s', e)
+			
+			
+	def getHashableImplicitInputs(self, context):
+		assert self.options
+		return [str(self.options)]
+		
+	def clean(self, context): 
+		pass
+
+pathset = CustomPathSet('test.xpybuild.py')
+t = MyTarget('${OUTPUT_DIR}/defaults.txt', deps=pathset)
+pathset.target = t
+
+MyTarget('${OUTPUT_DIR}/legacyTargetOverride.txt', options={'testoption.legacyTargetOverride':'${MY_PROP}'})
+MyTarget('${OUTPUT_DIR}/targetOverride.txt').option('testoption.targetOverride', 'expectedval')
+# do both and check .option(...) takes precedence
+MyTarget('${OUTPUT_DIR}/targetOverrideBoth.txt', options={'testoption.targetOverride':'unexpectedval'}).option('testoption.targetOverride', 'expectedval')
---
+++ tests/correctness/framework/Doctests/run.py
@@ -0,0 +1,64 @@
+from pysys.constants import *
+from xpybuild.xpybuild_basetest import XpybuildBaseTest
+
+class PySysTest(XpybuildBaseTest):
+
+	def execute(self):
+		dirs = list()
+		EXCLUDED_MODULES = []
+
+		i = 0
+		skipped = 0
+		failedModules = []
+		
+		DIR = PROJECT.XPYBUILD_ROOT
+		allScripts = []
+		for dirpath, dirnames, filenames in os.walk(DIR):
+			for excl in ['.svn', 'tests', 'release-output', 
+					# doesn't contain any
+					'targets']:
+				if excl in dirnames: dirnames.remove(excl)
+
+			for f in filenames:
+				if f.endswith('.py') and f != '__init__.py' and f != 'root.xpybuild.py':
+					with open(dirpath+'/'+f) as pyfile:
+						if '>>>' in pyfile.read():
+							allScripts.append(dirpath+'/'+f)
+		
+		good = bad = 0
+		for f in allScripts:
+			i += 1
+			moduleName = f.replace(DIR,'').replace('/','.').replace('\\','.').strip('.').replace('.py','')
+			
+			if any(map(lambda x: re.search(x, moduleName), EXCLUDED_MODULES)):
+				self.log.info("skipping excluded module %s"%moduleName)
+				skipped += 1
+				continue
+			
+			environs = {}#dict(os.environ)
+			if PLATFORM != 'win32':
+				pythonhome = os.path.dirname(sys.executable)
+				if pythonhome.endswith('bin'): pythonhome = os.path.dirname(pythonhome)
+				# python doesn't seem to launch on linux without PYTHONHOME being set
+				environs['PYTHONHOME'] = pythonhome
+			else:
+				environs['SYSTEMROOT'] = os.environ['SYSTEMROOT']
+			environs['PYTHONPATH'] = DIR
+			result = self.startProcess(sys.executable, ['-m', 'doctest', '-v', f], 
+				environs=environs, 
+				stdout=moduleName+'.out', stderr=moduleName+'.err', displayName='doctest '+moduleName, 
+				abortOnError=True, ignoreExitStatus=True)
+
+			if result.exitStatus != 0: 
+				self.logFileContents(moduleName+'.err', maxLines=0) or self.logFileContents(moduleName+'.out', maxLines=0)
+				self.addOutcome(FAILED, 'doctest %s failed'%moduleName, abortOnError=False)
+				bad += 1
+			else:
+				good += 1
+			#return # TODO - remove
+		assert good+bad > 0, 'some tests should have run'
+		self.log.info('Completed doctesting %d modules; %d failed', good+bad, bad)
+		self.log.info("%d modules were skipped" % skipped)
+		
+	def validate(self):
+		pass
