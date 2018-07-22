c4502645291f2ef1a1eaf08801fd4fa99e3beff2
Ben Spiller <ben-spiller@users.noreply.github.com>
2018-07-22 16:19:51 +0100

    add perf test for .exists() without caching

--- tests/performance/microbenchmarks/MicroPerf_FileUtilsCaches/run.py
+++ tests/performance/microbenchmarks/MicroPerf_FileUtilsCaches/run.py
@@ -4,5 +4,6 @@ from xpybuild.microperf_basetest import MicroPerfPySysTest
 class PySysTest(MicroPerfPySysTest):
 	OPERATIONS = [
 	# resultKey (must be a valid filename), command, setup
+	('fileutils.exists() non-existent file without caching',"utils.fileutils.exists(OUTPUT_DIR+'/doesntexist%d'%iteration)", ""),
 	('fileutils.exists() non-existent file with caching',"utils.fileutils.exists(OUTPUT_DIR+'/doesntexist')", ""),
 	]
