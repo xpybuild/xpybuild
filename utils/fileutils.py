56b7cdd13390c10204ba7fa9a5b623cb363e9ad2
Ben Spiller <ben-spiller@users.noreply.github.com>
2018-07-27 22:55:34 +0100

    optimization: isDirPath does not need to check \ on OSes where that separator isn't used

--- utils/fileutils.py
+++ utils/fileutils.py
@@ -261,12 +261,20 @@ def parsePropertiesFile(lines, excludeLines=None):
 		result.append((key,value, lineNo))
 	return result
 
-def isDirPath(path):
-	""" Returns true if the path is a directory (ends with / or \\). """
-	try:
-		return path[-1] in ('/', '\\')
-	except Exception:
-		return False
+if os.pathsep == '\\':
+	def isDirPath(path):
+		""" Returns true if the path is a directory (ends with / or \\). """
+		try:
+			return path[-1] in ('/', '\\')
+		except Exception:
+			return False
+else:
+	def isDirPath(path):
+		""" Returns true if the path is a directory (ends with / or \\). """
+		try:
+			return path[-1] == '/'
+		except Exception:
+			return False
 
 
 __longPathCache = {} # GIL protects integrity of dict, no need for extra locking as it's only a cache
