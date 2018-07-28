3c40a6a97703b863308f5d412735e6bc69f091e0
Ben Spiller <ben-spiller@users.noreply.github.com>
2018-07-28 14:53:29 +0100

    skip Docker test on windows as it doesn't work (yet)

--- tests/correctness/targets/Docker/run.py
+++ tests/correctness/targets/Docker/run.py
@@ -4,6 +4,7 @@ from xpybuild.xpybuild_basetest import XpybuildBaseTest
 class PySysTest(XpybuildBaseTest):
 
 	def execute(self):
+		if PLATFORM=='win32': self.abort(SKIPPED, 'docker currently not supported on Windows')
 		msg = self.xpybuild(shouldFail=False, args=['DOCKER_PATH=/usr/bin', 'DOCKER_HOST=', 'DOCKER_REPO=', 'DOCKER_USER='])
 
 	def validate(self):
