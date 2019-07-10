027dfdc929e783b3d8d587b9d8c6640673575f1a
ben-spiller <ben-spiller@users.noreply.github.com>
2019-07-10 20:13:20 +0100

    add a check that the incremental compilation still works to CppCompilation, especially as it has extra edge cases for Cpp files

--- tests/correctness/targets/CppCompilation/run.py
+++ tests/correctness/targets/CppCompilation/run.py
@@ -7,7 +7,11 @@ class PySysTest(XpybuildBaseTest):
 		msg = self.xpybuild(shouldFail=False, args=[], buildfile='root.xpybuild.py')
 		self.startProcess(self.output+'/build-output/test', [], stdout='test.out', stderr='test.err')
 
+		self.xpybuild(shouldFail=False, args=[], buildfile='root.xpybuild.py', stdouterr='xpybuild-incremental-build')
+
 	def validate(self):
 		self.assertGrep(file='xpybuild.out', expr="ERROR .*", contains=False)
 		self.assertGrep(file='test.out', expr="Got string: <STRING1STRING2>")
-		
+
+		# incremental should not rebuild anything
+		self.assertGrep(file='xpybuild-incremental-build.out', expr="XPYBUILD SUCCEEDED: <NO TARGETS> built .*2 up-to-date")
