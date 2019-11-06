43ee12876615ad633398e9702e1b38438e20d1ce
Matthew Johnson <github@matthew.ath.cx>
2019-11-06 12:11:27 +0000

    remove temp logging and don't look for 3 CPUs when there's only 2 in travis

--- tests/correctness/framework/TargetTimeLogging/run.py
+++ tests/correctness/framework/TargetTimeLogging/run.py
@@ -30,8 +30,6 @@ class PySysTest(XpybuildBaseTest):
 		# check , in input name doesn't break csv
 		self.assertGrep('timefile-output.csv', expr='test_target5')
 
-		self.logFileContents('xpybuild.log', maxLines=0) # temporary to see what's going on in travis
-
 		# check header and times roughly correct
 		N = 1.39728
 		NAME=0
@@ -89,12 +87,10 @@ class PySysTest(XpybuildBaseTest):
 			self.assertTrue(maxcrit < 6*N+1 and maxcrit > 6*N-1, assertMessage="Critical path is 6*N +/- 1 (it was %s)" % maxcrit)
 
 		# check cpu stats
-		self.logFileContents('xpybuild.log', maxLines=0)
 		self.assertOrderedGrep('xpybuild.log', exprList=[
 				r'Utilisation average: 01\.[0-9]*/%d'%multiprocessing.cpu_count(),
 				r'Utilisation histogram:',
 				r'1\] \([0-9\.]*[1-9][0-9\.]*%\) ==============',
 				r'2\] \([0-9\.]*[1-9][0-9\.]*%\) ==========',
-				r'3\] \([0-9\.]*[1-9][0-9\.]*%\) ======',
 			])
 
