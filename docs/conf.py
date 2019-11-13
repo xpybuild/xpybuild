2d6bcebea0acd9e49fa6ec9d1fb55b848d476514
ben-spiller <ben-spiller@users.noreply.github.com>
2019-11-13 13:49:51 +0000

    add read-the-docs workarounds to make the output look way nicer

--- docs/conf.py
+++ docs/conf.py
@@ -148,4 +148,9 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
+
+html_context = {'css_files': [
+	# Workaround for RTD 0.4.3 bug https://github.com/readthedocs/sphinx_rtd_theme/issues/117
+	'_static/theme_overrides.css',  # override wide tables in RTD theme
+]}
