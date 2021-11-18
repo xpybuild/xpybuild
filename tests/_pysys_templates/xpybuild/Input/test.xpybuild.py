7acb6bfe332b074f29b2d71a393a6d4d77a76b4d
ben-spiller <ben-spiller@users.noreply.github.com>
2021-11-18 10:53:04 +0000

    add pysys make template

---
+++ tests/_pysys_templates/xpybuild/Input/test.xpybuild.py
@@ -0,0 +1,15 @@
+from xpybuild.propertysupport import *
+from xpybuild.buildcommon import *
+from xpybuild.pathsets import *
+
+from xpybuild.targets.writefile import *
+from xpybuild.targets.copy import *
+
+defineOutputDirProperty('OUTPUT_DIR', None)
+
+class CustomTarget(WriteFile):
+	def run(self, context):
+		self.log.critical('Test message from build file at CRIT level')
+		return super(CustomTarget, self).run(context)
+
+CustomTarget('${OUTPUT_DIR}/output.txt', 'Test message')
