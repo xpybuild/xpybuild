f82757f031e0ffd6d2d37d855d2edb191ae59b1f
ben-spiller <ben-spiller@users.noreply.github.com>
2022-04-12 10:54:15 +0100

    try to avoid hardcoding VS version since GH Actions Win2022 only has latest

--- tests/build_utilities/native_config.xpybuild.py
+++ tests/build_utilities/native_config.xpybuild.py
@@ -1,19 +1,28 @@
-import os
+import os, glob, logging
 from xpybuild.propertysupport import *
 from xpybuild.buildcommon import *
 from xpybuild.pathsets import *
 from xpybuild.utils.compilers import GCC, VisualStudio
 
+log = logging.getLogger('xpybuild.tests.native_config')
+
 # some basic defaults for recent default compilers for running our testcases with
 if IS_WINDOWS:
-	VSROOT=r'c:\Program Files (x86)\Microsoft Visual Studio 14.0'
-	assert os.path.exists(VSROOT), 'Cannot find Visual Studio installed in: %s'%VSROOT
+	VSROOT=r'c:\Program Files (x86)\Microsoft Visual Studio *'
+	if glob.glob(VSROOT):
+		VSROOT = sorted(glob.glob(VSROOT))[-1] # pick the latest one
+	else:
+		raise Exception('Cannot find Visual Studio installed in: %s'%VSROOT)
 	
 	setGlobalOption('native.include', [
 		VSROOT+r"\VC\ATLMFC\INCLUDE", 
 		VSROOT+r"\VC\INCLUDE", 
 		r"C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\ucrt",
 	])
+	if not os.path.exists(r"C:\Program Files (x86)\Windows Kits\10"):
+		log.warning('WARN - Cannot find expected Windows Kits, got: %s'%sorted(glob.glob(r"C:\Program Files (x86)\Windows Kits\*")))
+	if not os.path.exists(r"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.10240.0\ucrtx"):
+		log.warning('WARN - Cannot find expected Windows Kits UCRT, got: %s'%sorted(glob.glob(r"C:\Program Files (x86)\Windows Kits\10\Lib\*\*")))
 	setGlobalOption('native.libpaths', [
 		VSROOT+r"\VC\ATLMFC\LIB\amd64", 
 		VSROOT+r"\VC\LIB\amd64", 
