d2e1337f02ea86cc96039c531bb955b4801bc462
ben-spiller <ben-spiller@users.noreply.github.com>
2025-10-03 11:31:13 +0100

    use globbing to automatically detect VS location in tests

--- tests/build_utilities/native_config.xpybuild.py
+++ tests/build_utilities/native_config.xpybuild.py
@@ -8,6 +8,8 @@ log = logging.getLogger('xpybuild.tests.native_config')
 
 # some basic defaults for recent default compilers for running our testcases with
 if IS_WINDOWS:
+	# A more standard approach would be to execute vswhere.exe (from its well-known location) - but this still doesn't
+	# give us the cl.exe
 	__vspatterns=[r'c:\Program Files (x86)\Microsoft Visual Studio 1*', r'C:\Program Files\Microsoft Visual Studio\*\Enterprise']
 	__vsfound = sorted(glob.glob(__vspatterns[0]))+sorted(glob.glob(__vspatterns[1]))
 	log.critical('Found these VS installations: %s', __vsfound)
@@ -18,30 +20,38 @@ if IS_WINDOWS:
 	else:
 		raise Exception('Cannot find Visual Studio installed in: %s'%__vspatterns)
 	
+	def findLatest(pattern):
+		results = sorted(glob.glob(pattern))
+		if results: return results[-1]
+		log.warning('Cannot find any path matching: %s', pattern)
+		return pattern.replace('*', '[missing]')
+
 	setGlobalOption('native.include', [
 		VSROOT+r"\VC\ATLMFC\INCLUDE", 
 		VSROOT+r"\VC\INCLUDE", 
-		r"C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\ucrt",
+		findLatest(r"C:\Program Files (x86)\Windows Kits\10\Include\*\ucrt"),
 	])
 	if not os.path.exists(r"C:\Program Files (x86)\Windows Kits\10"):
 		log.warning('WARN - Cannot find expected Windows Kits, got: %s'%sorted(glob.glob(r"C:\Program Files (x86)\Windows Kits\*")))
-	if not os.path.exists(r"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.10240.0\ucrt"):
-		log.warning('WARN - Cannot find expected Windows Kits UCRT, got: %s'%sorted(glob.glob(r"C:\Program Files (x86)\Windows Kits\10\Lib\*\*")))
+	#if not os.path.exists(r"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.10240.0\ucrt"):
+	#	log.warning('WARN - Cannot find expected Windows Kits UCRT, got: %s'%sorted(glob.glob(r"C:\Program Files (x86)\Windows Kits\10\Lib\*\*")))
 	setGlobalOption('native.libpaths', [
 		VSROOT+r"\VC\ATLMFC\LIB\amd64", 
 		VSROOT+r"\VC\LIB\amd64", 
-		r"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.10240.0\ucrt\x64", 
-		r"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64",
+		findLatest(r"C:\Program Files (x86)\Windows Kits\10\Lib\*\ucrt\x64"),
+		findLatest(r"C:\Program Files (x86)\Windows Kits\10\Lib\*\um\x64"),
 	])
 	setGlobalOption('native.cxx.path', [
 		VSROOT+r"\Common7\IDE",
 		VSROOT+r"\VC\BIN\amd64", 
 		VSROOT+r"\Common7\Tools", 
-		r"c:\Windows\Microsoft.NET\Framework\v3.5",
+		findLatest(r"c:\Windows\Microsoft.NET\Framework\v*"), # was 3.5
 	])
 	
-	# If we wanted to support VS 2022 we'd need to update this path
-	setGlobalOption('native.compilers', VisualStudio(VSROOT+r'\VC\bin\amd64'))
+	toolsBin = VSROOT+r'\VC\bin\amd64' # used for old VS versions like 2019
+	if not os.path.exists(toolsBin):
+		toolsBin = findLatest(VSROOT+r'\VC\Tools\MSVC\*\bin\Hostx64\x64')
+	setGlobalOption('native.compilers', VisualStudio(toolsBin))
 	setGlobalOption('native.cxx.flags', ['/EHa', '/GR', '/O2', '/Ox', '/Ot', '/MD', '/nologo'])
 	
 else:
